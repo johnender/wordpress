33ff4eea68900b6f26ed1ff4a86f70ec460b8cc3
John Hernández <johnenderhernandez@gmail.com>
2021-04-05 04:17:03 -0600

    agregando un template

---
+++ platzitheme-comentado/template-institucional.php
@@ -0,0 +1,24 @@
+<?php /**
+ * Template Name: Página institucional
+ */
+
+get_header(); ?>
+<!-- pasando el header del tema -->
+
+<main class='container'>
+    <?php if(have_posts()){     //revisa si hay informacion para mostrar
+            while(have_posts()){
+                //la funcion the_post() muestra el contenido y ademas instancia cada vuelta del loop (suma y al final da false)
+                the_post(); ?>  
+
+                <!-- se usan las funciones de wordpress que devuelven el titulo y el contenido -->
+            <h1 class='my-3'>Pagina: <?php the_title(); ?></h1>
+
+            <?php the_content(); ?>
+
+         <?php }
+    }?>
+</main>
+
+<?php get_footer(); ?>
+<!-- pasando el footer del tema -->
